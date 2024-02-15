0745e4a02b864f33ccaf7fc19d931c0c1786c3b6
Joseph Kligel <jkligel@gmail.com>
2024-02-14 21:20:00 -0600

    Ch. 10...

---
+++ traits/src/lib.rs
@@ -0,0 +1,78 @@
+pub mod summary;
+
+use crate::summary::Summary;
+//use std::fmt::Display;
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize_author(&self) -> String {
+        //format!("{}, by {} ({})", self.headline, self.author, self.location)
+        format!("@{}", self.author)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+//pub fn notify(item: &(impl Summary + Display) ) {
+//pub fn notify<T: Summary + Display>(item: &T) {
+//fn some_function<T, U>(t: &T, u: &U) -> i32
+//where
+//    T: Display + Clone,
+//    U: Clone + Debug,
+//{
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+pub fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people"
+        ),
+        reply: false,
+        retweet: false,
+    }
+}
+
+// Using Trait Bounds to Conditionally Implement Methods
+
+use std::fmt::Display;
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self {x,y}
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}

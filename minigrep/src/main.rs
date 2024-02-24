8bdcc5e0e0c28ff45ac680979967eca3656d0623
Joseph Kligel <jkligel@gmail.com>
2024-02-23 22:18:23 -0600

    Finish Ch.12

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,7 +1,5 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
 
 use minigrep::Config;
 
@@ -9,15 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }

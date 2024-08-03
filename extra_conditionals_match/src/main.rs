e5f9ae02cb7e444bb4575703359c8575a8f93e47
Joseph Kligel <jkligel@gmail.com>
2024-08-03 03:51:38 -0500

    Finish Ch. 19 of Rust Book

---
+++ extra_conditionals_match/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number {x} is odd"),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}"); 
+}
